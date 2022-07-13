da2a2246746bed022b5b3f8169a8c40def4b368c
Sool Uh! <suluh.webdevelopers@hotmail.com>
2022-07-13 10:04:51 +0700

    :pencil: renew

--- src/card.js
+++ src/card.js
@@ -1,107 +1,118 @@
 #!/usr/bin/env node
-"use strict";
+'use strict'
 
-const inquirer = require("inquirer");
-const request = require("request");
-const boxen = require("boxen");
-const chalk = require("chalk");
-const clear = require("clear");
-const open = require("open");
-const path = require("path");
-const ora = require("ora");
-const fs = require("fs");
+const fs = require('fs')
+const ora = require('ora')
+const path = require('path')
+const open = require('open')
+const clear = require('clear')
+const chalk = require('chalk')
+const boxen = require('boxen')
+const request = require('request')
+const inquirer = require('inquirer')
 
-clear();
-const prompt = inquirer.createPromptModule();
-const data = require("./data.json");
+clear()
+const prompt = inquirer.createPromptModule()
+const data = require('./data.json')
 
 const questions = [
-    {
-        type: "list",
-        name: "action",
-        message: "What do you want to do?",
-        choices: [
-            {
-                name: `Send me an ${chalk.green.bold("email")}?`,
-                value: () => {
-                    open(`mailto:${data.email}`);
-                    console.log("Done, see you in the inbox.\n");
-                }
-            },
-            {
-                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
-                value: () => {
-                    let loader = ora({
-                        text: "Downloading Resume"
-                    }).start();
+  {
+    type: 'list',
+    name: 'action',
+    message: 'What do you want to do?',
+    choices: [
+      {
+        name: `Send me an ${chalk.green.bold('email')}?`,
+        value: () => {
+          open(`mailto:${data.email}`)
+          console.log('Done, see you in the inbox.\n')
+        }
+      },
+      {
+        name: `Download my ${chalk.magentaBright.bold('Resume')}?`,
+        value: () => {
+          let loader = ora({
+            text: 'Downloading Resume'
+          }).start()
 
-                    let download = request(data.resume);
-                    let filename = data.resume.substring(data.resume.lastIndexOf("/") + 1);
-                    download.pipe(fs.createWriteStream(`./${filename}`));
+          setTimeout(() => {
+            loader.stop()
 
-                    download.on("complete", () => {
-                        let downloadPath = path.join(process.cwd(), filename);
-                        loader.stop();
+            const sorry = [
+              '\nHello, I\'m sorry, I appreciate your curiosity.',
+              'But for privacy, I can\'t just share my CV with strangers.',
+              'Please contact me via WhatsApp.\n'
+            ]
+            sorry.map((message) => console.log(chalk.italic(message)))
 
-                        console.log(`Resume Downloaded at ${downloadPath} \n`);
-                        open(downloadPath);
-                    });
-                }
-            },
-            {
-                name: "Just quit.",
-                value: () => {
-                    console.log("See you later.\n");
-                }
-            }
-        ]
-    }
-];
+            open(data.biolink)
+          }, 1_000)
+          return
 
-const boxData = {
-    name: chalk.green.bold(`              ${data.full_name}`),
-    work: chalk.white(`${data.profession} at `) + chalk.hex("#2B82B2").bold(data.company),
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan(data.socials.twitter),
-    github: chalk.gray("https://github.com/") + chalk.green(data.socials.github),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue(data.socials.linkedin),
-    web: chalk.cyan(data.website),
-    npx: chalk.red("npx ") + chalk.white(data.package),
+          let download = request(data.resume)
+          let filename = data.resume.substring(data.resume.lastIndexOf('/') + 1)
+          download.pipe(fs.createWriteStream(`./${filename}`))
 
-    labelWork: chalk.white.bold("       Work:  "),
-    labelTwitter: chalk.white.bold("    Twitter:  "),
-    labelGitHub: chalk.white.bold("     GitHub:  "),
-    labelLinkedIn: chalk.white.bold("   LinkedIn:  "),
-    labelWeb: chalk.white.bold("        Web:  "),
-    labelCard: chalk.white.bold("       Card:  ")
-};
+          download.on('complete', () => {
+            let downloadPath = path.join(process.cwd(), filename)
+            loader.stop()
 
-let descriptions = data.description.map(value => chalk.italic(value)).join(`\n`);
-let me = [
-    boxData.name,
-    "",
-    boxData.labelWork + boxData.work,
-    "",
-    boxData.labelTwitter + boxData.twitter,
-    boxData.labelGitHub + boxData.github,
-    boxData.labelLinkedIn + boxData.linkedin,
-    boxData.labelWeb + boxData.web,
-    "",
-    boxData.labelCard + boxData.npx,
-    ""
-];
-me = me.concat(descriptions);
+            console.log(`Resume Downloaded at ${downloadPath} \n`)
+            open(downloadPath)
+          })
+        }
+      },
+      {
+        name: 'Just quit.',
+        value: () => {
+          console.log('See you later.\n')
+        }
+      }
+    ]
+  }
+]
 
-const box = boxen(
-    me.join(`\n`),
-    {
-        margin: 1,
-        float: "center",
-        padding: 1,
-        borderStyle: "single",
-        borderColor: "green"
-    }
-);
-console.log(box);
-console.log(`Tip: Try ${chalk.cyanBright.bold("cmd/ctrl + click")} on the links above\n`);
+const box = {
+  label: {
+    work: chalk.white.bold('       Work:  '),
+    twitter: chalk.white.bold('    Twitter:  '),
+    github: chalk.white.bold('     GitHub:  '),
+    linkedin: chalk.white.bold('   LinkedIn:  '),
+    web: chalk.white.bold('        Web:  '),
+    card: chalk.white.bold('       Card:  '),
+  },
 
-prompt(questions).then(answer => answer.action());
+  name: chalk.green.bold(`              ${data.fullname}`),
+  work: chalk.white(`${data.profession} at `) + chalk.hex('#ffc300').bold(data.company),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan(data.socials.twitter),
+  github: chalk.gray('https://github.com/') + chalk.green(data.socials.github),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue(data.socials.linkedin),
+  web: chalk.cyan(data.website),
+  npx: chalk.red('npx ') + chalk.white(data.package),
+}
+
+const me = [
+  box.name,
+  '',
+  box.label.work + box.work,
+  '',
+  box.label.twitter + box.twitter,
+  box.label.github + box.github,
+  box.label.linkedin + box.linkedin,
+  box.label.web + box.web,
+  '',
+  box.label.card + box.npx,
+  '',
+  ...data.description.map(value => chalk.italic(value))
+]
+
+console.log(boxen(me.join(`\n`), {
+  margin: 1,
+  float: 'center',
+  padding: 1,
+  borderStyle: 'single',
+  borderColor: 'green'
+}))
+console.log(`Tip: Try ${chalk.cyanBright.bold('cmd/ctrl + click')} on the links above\n`)
+
+prompt(questions).then(answer => answer.action())
